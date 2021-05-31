bf47c08461a1dd74912eed9068d548a38c8a5c32
Michael Bowen <mikee.bowen@gmail.com>
2021-05-31 10:50:24 -0700

    update typescript

--- dist/writer.js
+++ dist/writer.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Writer = void 0;
 class Writer {
     write(message = 'Hello World!') {
         console.log(message);
